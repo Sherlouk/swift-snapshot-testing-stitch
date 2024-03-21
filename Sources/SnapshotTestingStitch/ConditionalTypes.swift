62e3833709bf30557bcdad081828e5fba66a5e8a
Seyed Mojtaba Hosseini Zeidabadi <Chenzook+GitHub@gmail.com>
2024-03-21 15:19:46 +0330

    feat: add conditional Color type

--- Sources/SnapshotTestingStitch/ConditionalTypes.swift
+++ Sources/SnapshotTestingStitch/ConditionalTypes.swift
@@ -1,9 +1,11 @@
 #if canImport(UIKit)
 import UIKit
+public typealias Color = UIColor
 public typealias Font = UIFont
 public typealias ViewController = UIViewController
 #elseif canImport(AppKit)
 import AppKit
+public typealias Color = NSColor
 public typealias Font = NSFont
 public typealias ViewController = NSViewController
 #endif
